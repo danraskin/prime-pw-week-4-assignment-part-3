5f91b561d6f7d812e50a5ca07dcaae4251403109
Dan Raskin <Danieltraskin@gmail.com>
2022-08-03 12:59:59 -0500

    finished stretch problem 3

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -4,6 +4,7 @@ console.log('***** Cart Functions *****');
 
 var basket = [];
 let oldItems = [];
+const maxItems = 5;
 
 //   - Create a function called `addItem`. It should:
 //   - take an input parameter for a string `item`
@@ -30,6 +31,7 @@ addItem('dog');
 addItem('lemur');
 addItem('gerbil');
 addItem('spittle');
+addItem('sawdust');
 
 console.log(`The items my basket are: ${basket}`);
 
@@ -50,3 +52,35 @@ console.log('all your old items are here: ', oldItems);
 // this was took me a minute. I forgot that the for loop *keeps running as long as second condition is TRUE* 
 //rather than *keeps running until the second condition is true*
 //I also forgot my terminating condition did not need to involve i!
+
+// 2. Create a function called isFull(). It should:
+//   - return `false` if the basket contains *less* than max number of items
+//   - return `true` otherwise (equal or more than maxItems)
+
+function isFull(items) {
+    if (items.length < maxItems) {
+        return false;
+    }   return true;
+}
+
+console.log('when basket is empty, return false: ', isFull(basket));
+console.log('wastebasket has all the old items from old basket, return true: ', isFull(oldItems));
+
+// 3. Update the required `addItem` function to:
+//   - Use the `isFull` function to prevent more than `maxItems` from being added to the basket. 
+//   - If an item was added to the array, return `true`
+//   - If there was no room and the item could not be added return `false`
+
+function addItem2 (item) {
+    if (isFull(basket) === false) {
+        basket.push(item)
+        return true;
+    }   return false;
+}
+console.log('Adding stuff (expect true)', addItem2('stuff1'));
+console.log('Adding stuff (expect true)', addItem2('stuff2'));
+console.log('Adding stuff (expect true)', addItem2('stuff3'));
+console.log('Adding stuff (expect true)', addItem2('stuff4'));
+console.log('Adding stuff (expect true)', addItem2('stuff5'));
+console.log('Adding stuff (expect false)', addItem2('stuff6'));
+console.log(basket);
